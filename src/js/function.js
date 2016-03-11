62e47853aa928429d5cad3e85c9bd49e0d3f00b1
Martijn <m.reeuwijk@live.nl>
2016-03-11 10:40:42 +0100

    sidemenu

--- src/js/function.js
+++ src/js/function.js
@@ -13,30 +13,37 @@ $( document ).ready(function() {
 	});
 
 	$('#yourdest').keypress(function (e) {
-	  if (e.which == 13) {
-		$('#yourloc').submit();
-	    $('#yourdest').submit();
-		$("#yourdest").select().blur();
-		$('.layerbtn').toggleClass('scale');
-		$('.mapmenu').toggleClass('fullscreen');
+		if (e.which == 13) {
+			$('#yourloc').submit();
+			$('#yourdest').submit();
+			$("#yourdest").select().blur();
+			$('.layerbtn').toggleClass('scale');
+			$('.mapmenu').toggleClass('fullscreen');
 
-	  }
+		}
 	});
 
 	$('#yoursearch').keypress(function (e) {
-	  if (e.which == 13) {
-		$('#yoursearch').submit();
-		$("#yoursearch").select().blur();
-		$('.sidemenu').toggleClass('leftmove');
-		$('.first').toggleClass('disnone');
+		if (e.which == 13) {
+			$('#yoursearch').submit();
+			$("#yoursearch").select().blur();
+			$('.sidemenu').toggleClass('leftmove');
+			$('.first').toggleClass('disnone');
+			$('.collapsible').toggleClass('disnone');
 
-	  }
+		}
 	});
 
 	$('.navbtn').click(function(){
 		$('.locframe').toggleClass('pushleft');
 		$('.first').toggleClass('pushleft');
-		// $('.first').toggleClass('disnone');
+		$('.sidemenu').removeClass('leftmove');
+	});
+
+	$(document).ready(function(){
+		$('.collapsible').collapsible({
+			accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+		});
 	});
 
 });
